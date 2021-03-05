26f1f290e9e1f855808d0e729837997b497cad73
starfrost <mario64crashed@gmail.com>
2021-03-05 21:30:32 +0000

    Artifact creation test

--- Lightning.Utilities/Error/Error.cs
+++ Lightning.Utilities/Error/Error.cs
@@ -4,11 +4,20 @@ using System.Text;
 
 namespace Lightning.Utilities
 {
+    /// <summary>
+    /// Lightning
+    /// 
+    /// Error
+    /// 
+    /// Non-instanceable object
+    /// </summary>
     public class Error
     {
         public delegate bool Handler(Error err);
         public string Description { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
+
 
     }
 }
