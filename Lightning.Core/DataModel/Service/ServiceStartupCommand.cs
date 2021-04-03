61c0e91c2b3f86f12556adb4dd101b59e068d8c9
starfrost <mario64crashed@gmail.com>
2021-04-04 00:35:18 +0100

    Add refactored StaticSerialiser from Iris [Trackmaker 2.1] and begin work on globalsettings

---
+++ Lightning.Core/DataModel/Service/ServiceStartupCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lightning.Core
+{
+    /// <summary>
+    /// Non-DataModel (Instance Core)
+    /// 
+    /// ServiceStartupCommand
+    /// 
+    /// 2021-04-03
+    /// 
+    /// Holds information for instructing the SCM to strt a service at system boot time. 
+    /// </summary>
+    public class ServiceStartupCommand
+    {
+        /// <summary>
+        /// The start order of this service.
+        /// </summary>
+        public int StartOrder { get; set; }
+        public string SvcName { get; set; }
+    }
+}
---
+++ Lightning.Core/GlobalSettings/GlobalSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lightning.Core
+{ 
+    /// <summary>
+    /// Non-DataModel (Engine Core)
+    /// 
+    /// GlobalSettings
+    /// 
+    /// 2021-04-03
+    /// 
+    /// Holds global settings for the engine.
+    /// 
+    /// uses Lightning.Core.StaticSerialiser.dll
+    /// </summary>
+    public static class GlobalSettings
+    {
+        public static List<ServiceStartupCommand> ServiceStartupCommands { get; set; }
+    }
+}
