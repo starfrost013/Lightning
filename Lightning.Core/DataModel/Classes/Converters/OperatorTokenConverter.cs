7f1c263131142f383ea6b2562016841617465b67
starfrost <mario64crashed@gmail.com>
2021-04-22 22:59:14 +0100

    fixed compile

--- Lightning.Core/DataModel/Classes/Converters/OperatorTokenConverter.cs
+++ Lightning.Core/DataModel/Classes/Converters/OperatorTokenConverter.cs
@@ -24,7 +24,7 @@ namespace Lightning.Core.API
             }
             else
             {
-                return base.CanConvertTo(context, destinationType;
+                return base.CanConvertTo(context, destinationType);
             }
             
         }
